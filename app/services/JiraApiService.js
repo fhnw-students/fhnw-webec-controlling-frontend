216fc7cb52b7674cbd2ca532c4a614f2bc149d93
hirsch88 <gery.hirschfeld@w3tec.ch>
2016-05-26 18:06:04 +0200

    finishes the jira api service

--- app/services/JiraApiService.js
+++ app/services/JiraApiService.js
@@ -5,26 +5,68 @@ define(['jquery', 'services/SessionService'], function ($, SessionService) {
 	/**
 	 * This is the default jira backend uri
 	 */
-	var baseUrl = 'https://www.cs.technik.fhnw.ch/jira/rest/api/2';
+	var baseUrl = 'https://www.cs.technik.fhnw.ch/jira/rest';
+	/**
+	 * Route to the main jira api
+	 */
+	var jiraRoute = '/api/2';
+	/**
+	 * Route to the jira plugin tempo
+	 */
+	var tempoRoute = '/tempo-timesheets/3/worklogs';
   /**
    * Public API
    * All the returned function are available from outside
    */
   return {
-		getAllProjects: getAllProjects
+		getAllProjects: getAllProjects,
+		getIssue: getIssue,
+		getWorklogs: getWorklogs
   };
   //////////////////////////////////////////////////////////////////////////////////
 	/**
 	 * Request all jira projects of the user
+	 *
+	 * @returns {Promise<Object[]>} Collection of Jira projects
 	 */
 	function getAllProjects() {
-		return request('/project');
+		return requestJira('/project');
+	}
+	/**
+	 * Request the issue with the given key
+	 *
+	 * @param  {string} key
+	 * @returns {Promise<Object>} Jira issue
+	 */
+	function getIssue(key) {
+		return requestJira('/issue', key);
+	}
+	/**
+	 * @param  {any} projectKey
+	 * @param  {any} dateFrom
+	 * @param  {any} dateTo
+	 * @returns {Promise<Object[]>} Collection of Jira worklogs
+	 */
+	function getWorklogs(projectKey, dateFrom, dateTo) {
+		var url = tempoRoute + '/?projectKey=' + projectKey + '&dateFrom=' + dateFrom + '&dateTo=' + dateTo;
+		return request(url);
 	}
 	/**
 	 * Makes a requst to the jira api backend
 	 *
 	 * @param  {string} url - rest api path
-	 * @return {Promise<Object[]>} Collection
+	 * @param  {string} key - identifier
+	 * @returns  {Promise<Object[]>} Collection
+	 */
+	function requestJira(url, key) {
+		var url = jiraRoute + url + (key) ? '/' + key : '',
+		return request(url);
+	}
+	/**
+	 * Makes a requst to the jira api backend
+	 *
+	 * @param  {string} url - rest api path
+	 * @returns {Promise<Object[]>} Collection
 	 */
 	function request(url) {
 		return new Promise(function (resolve, reject) {
